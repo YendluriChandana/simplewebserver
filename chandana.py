b34023b6104ed41a07471e5951fd4fb50be2bf1f
yendluri chandana <yenchanindian63@gmail.com>
2024-03-12 11:46:33 +0530

    Success

---
+++ chandana.py
@@ -0,0 +1,63 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Top 5 Revenue Generating Software Companies</title>
+</head>
+<body bgcolor="lavender">
+<table border="5" cellspacing ="6" align="center">
+<caption>Top 5 Revenue Generating Software Companies</caption>
+<tr>
+<th>Rank</th>
+<th>Company</th>
+<th>sales</th>
+<th>Nationality</th>
+</tr>
+<tr>
+<td>1</td>
+<td>Microsoft</td>
+<td>57.9</td>
+<td>USA</td>
+</tr>
+<tr>
+<td>2</td>
+<td>Oracle</td>
+<td>21.0</td>
+<td>USA</td>
+</tr>
+
+<tr>
+<td>3</td>
+<td>SAP</td>
+<td>16.1</td>
+<td>Germany</td>
+</tr>
+<tr>
+<td>4</td>
+<td>Computer Associates</td>
+<td>4.2</td>
+<td>USA</td>
+</tr>
+<tr>
+<td>5</td>
+<td>Adobe</td>
+<td>3.4</td>
+<td>USA</td>
+</tr>
+
+</body>
+</html>
+
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
